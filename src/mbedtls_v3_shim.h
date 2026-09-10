f0178d0a43703e4de4d69279ba1d547ed8b70bbb
aIecxs <alecxs@gmx.de>
2026-09-10 13:24:30 +0200

    dummy header

---
+++ src/mbedtls_v3_shim.h
@@ -0,0 +1,17 @@
+#ifndef MBEDTLS_V3_SHIM_H
+#define MBEDTLS_V3_SHIM_H
+
+/*
+ * Dummy header for the mbedtls_v3_shim Arduino library.
+ * The actual compatibility headers are included individually by applications.
+ */
+
+#ifndef MBEDTLS_ALLOW_PRIVATE_ACCESS
+#define MBEDTLS_ALLOW_PRIVATE_ACCESS
+#endif
+
+#ifndef MBEDTLS_DECLARE_PRIVATE_IDENTIFIERS
+#define MBEDTLS_DECLARE_PRIVATE_IDENTIFIERS
+#endif
+
+#endif
